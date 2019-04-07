19044fb4a2f8ba7c4317936a2e3fc7f8a0400ecd
JoseBuitrago <36308485+JoseBuitrago@users.noreply.github.com>
2019-04-07 15:33:47 -0500

    ChainOfResponsability

---
+++ ChainOfResponsability/src/main/java/ingsw/pdd/chainofresponsability/validator/CustomerValidator.java
@@ -0,0 +1,19 @@
+package ingsw.pdd.chainofresponsability.validator;
+
+import ingsw.pdd.chainofresponsability.domain.Customer;
+import ingsw.pdd.chainofresponsability.domain.order.AbstractOrder;
+
+
+public class CustomerValidator extends AbstractOrderValidator{
+    
+    
+    public void validate(AbstractOrder order) throws ValidationException{
+        for(AbstractOrderValidator validator : validators){
+            validator.validate(order);
+        }
+        
+        if(!(order.getContributor() instanceof Customer)){
+            throw new ValidationException("El contribuyente no es un cliente\"");
+        }
+    }
+}
