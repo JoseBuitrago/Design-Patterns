1b08b99d0ef3da4d0926b38fadc46733093d1cef
JoseBuitrago <36308485+JoseBuitrago@users.noreply.github.com>
2019-05-13 22:20:28 -0500

    Observer

---
+++ Observer/src/main/java/ingsw/pdd/observeer/ObserverMain.java
@@ -0,0 +1,45 @@
+package ingsw.pdd.observeer;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Observable;
+import ingsw.pdd.observer.impl.ConfigurationManager;
+import ingsw.pdd.observer.impl.observers.DateFormatObserver;
+import ingsw.pdd.observer.impl.observers.MoneyFormatObserver;
+
+public class ObserverMain {
+	
+	public static void main(String[] args) {
+        ConfigurationManager conf = ConfigurationManager.getInstance();
+        
+        //Se establecen los valores por default.
+        conf.setDefaultDateFormat(new SimpleDateFormat("yyyy/MM/dd"));
+        conf.setMoneyFormat(new DecimalFormat("##.00"));
+        System.out.println("Configuracion Establecida");
+        
+        //Se dan de alta lo observers
+        DateFormatObserver dateFormatObserver = new DateFormatObserver();
+        MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
+        conf.addObserver(dateFormatObserver);
+        conf.addObserver(moneyFormatObserver);
+        System.out.println("");
+        
+        //Se cambia la fonfiguratión
+        conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+        conf.setMoneyFormat(new DecimalFormat("###,#00.00"));
+        System.out.println("");
+        
+        //Se realiza otro cambio en la configuración.
+        conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy/dd"));
+        conf.setMoneyFormat(new DecimalFormat("###,#00"));
+        
+        conf.removeObserver(dateFormatObserver);
+        conf.removeObserver(moneyFormatObserver);
+        System.out.println("");
+        
+        //Se realiza otro cambio en la configuración.
+        conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy"));
+        conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
+    }
+}
+
---
+++ Observer/src/main/java/ingsw/pdd/observer/impl/IObservable.java
@@ -0,0 +1,10 @@
+package ingsw.pdd.observer.impl;
+
+public interface IObservable {
+
+    public void addObserver(IObserver observer);
+
+    public void removeObserver(IObserver observer);
+
+    public void notifyAllObservers(String command, Object source);
+}
